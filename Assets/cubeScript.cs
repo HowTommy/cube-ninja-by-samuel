a2d66b3a38b5cecb1fa28d55271f604e24a82b5b
HowTommy <howtommy.net@gmail.com>
2019-11-14 11:34:40 +0100

    Pushed the code

---
+++ Assets/cubeScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    float speed = 0.3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(Vector3.left * speed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(Vector3.right * speed);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(Vector3.up * speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(Vector3.down * speed);
+        }
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/ennemyscript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ennemyscript : MonoBehaviour
+{
+    public static int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            Debug.Log("Perdu");
+            SceneManager.LoadScene(1);
+        }
+        else if(other.name == "Down")
+        {
+            score++;
+            this.transform.position = new Vector3(Random.Range(-7.8f, 7.8f), 6.75f, this.transform.position.z);
+        }
+    }
+}
